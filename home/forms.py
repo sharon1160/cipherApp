b21fa53a26f706b38d4b3cdf0de4299c60cbd286
Sharon CR <schullunquia@unsa.edu.pe>
2021-11-02 10:19:51 -0500

    change output field

--- home/forms.py
+++ home/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 
 class TextTransForm(forms.Form):
-  key = forms.CharField(label='Clave ', max_length=200)
+  key = forms.CharField(label='Clave ', max_length=500)
   plain_text = forms.CharField(label='Texto Plano ',widget=forms.Textarea(attrs={'rows':3}))
 
 class TextPlayfairForm(forms.Form):
-  key = forms.CharField(label='Clave ', max_length=200)
+  key = forms.CharField(label='Clave ', max_length=500)
   text_cipher = forms.CharField(label='Texto Cifrado ',widget=forms.Textarea(attrs={'rows':3}))
 
 class TextCesarForm(forms.Form):
-  key = forms.CharField(label='Clave ', max_length=200)
+  key = forms.CharField(label='Clave ', max_length=500)
   text_cipher = forms.CharField(label='Texto Cifrado ',widget=forms.Textarea(attrs={'rows':3}))
 
 
