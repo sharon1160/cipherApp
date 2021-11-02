06f5b28f4da83b5d84388ce2f20529bfd09d3d21
Brayan Lipe <blipe@unsa.edu.pe>
2021-11-02 13:02:48 -0500

    Cesar's code 2.0

--- home/algorithms/Cesitar.py
+++ home/algorithms/Cesitar.py
@@ -1,25 +1,27 @@
 import string
 
-alfabeto = list(string.ascii_lowercase + string.ascii_uppercase)
-
+#alfabeto = list(string.ascii_lowercase + string.ascii_uppercase)
+alfabeto = list(string.ascii_uppercase)
+#alfabeto = list(string.ascii_lowercase)
 
 def cesar_cifrar(text, key):
+    text = text.upper()
     coded_text = ''
     for letra in text:
         if letra in alfabeto:
             indice_actual = alfabeto.index(letra)
             indice_cesar = indice_actual + key
 
-            if indice_cesar>42:
-                indice_cesar -= 42
+            if indice_cesar>27:
+                indice_cesar -= 27
             coded_text += alfabeto[indice_cesar]
         else:
             coded_text += letra
     
     return coded_text
 
-
 def cesar_descifrar(text, key):
+    #text = text.upper()
     decoded_text = ''
     for letra in text:
         if letra in alfabeto:
@@ -27,7 +29,7 @@ def cesar_descifrar(text, key):
             indice_original = indice_cesar - key
 
             if indice_original<0:
-                indice_original += 42
+                indice_original += 27
 
             decoded_text += alfabeto[indice_original]
         else:
