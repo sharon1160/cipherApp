d80fd65b42058223b7286771a64d7f83e11ea60b
Kemely Castillo <kcastillo@unsa.edu.pe>
2021-11-02 09:15:34 -0500

    ModificaPlay

--- home/algorithms/Playfair.py
+++ home/algorithms/Playfair.py
@@ -22,68 +22,61 @@ def matrix_clave(clave):
             j=0
     return matriz
 
+def DesencriptarPlayfair(clave,cifrado):
+    clave=clave.lower()
+    matriz =  matrix_clave(clave)
+    cifrado = cifrado.lower()
+    texto=[]
+    pares=[]
+    i=0
+    while (i<len(cifrado)):
+        a = cifrado[i]
+        b = cifrado[i+1]
+        pares.append(a+b)
+        i+=2
+    for par in pares:
+        f = False
+        c1 = par[0]
+        c2 = par[1]
+        for fila in matriz:
+            if c1 in fila and c2 in fila:
+                m1 = fila.find(c1)
+                m2 = fila.find(c2)
+                text = fila[(m1+4)%5]+fila[(m2+4)%5]
+                texto.append(text)
+                f = True
+        if f:
+            continue
+        for j in range(5):
+            col ="".join([matriz[i][j] for i in range(5)])
+            if c1 in col and c2 in col:
+                m1 = col.find(c1)
+                m2 = col.find(c2)
+                text = col[(m1+4)%5]+col[(m2+4)%5]
+                texto.append(text)
+                f = True
+        if f:
+            continue
+        i0=0
+        i1=0
+        j0=0
+        j1=0
+        for i in range(5):
+            fila = matriz[i]
+            if c1 in fila:
+                i0=i
+                j0=fila.find(c1)
+            if c2 in fila:
+                i1=i
+                j1=fila.find(c2)
+        text = matriz[i0][j1]+matriz[i1][j0]
+        texto.append(text)
+    #print('Texto: '+"".join(texto))
+    print(texto)
+    
 clav = input('Clave : ')
 cifrad = input('Texto cifrado : ')
-clave=clav.lower()
-matriz =  matrix_clave(clave)
-cifrado = cifrad.lower()
-texto=[]
-pares=[] 
-
-i=0
-while (i<len(cifrado)):
-    a = cifrado[i]
-    b = cifrado[i+1]
-    pares.append(a+b)
-    i+=2
-
-
-for par in pares:
-    f = False
-    c1 = par[0]
-    c2 = par[1]
-    for fila in matriz:
-        if c1 in fila and c2 in fila:
-            m1 = fila.find(c1)
-            m2 = fila.find(c2)
-            text = fila[(m1+4)%5]+fila[(m2+4)%5]
-            texto.append(text)
-            f = True
-    if f:
-        continue
-
-    for j in range(5):
-        col ="".join([matriz[i][j] for i in range(5)])
-        if c1 in col and c2 in col:
-            m1 = col.find(c1)
-            m2 = col.find(c2)
-            text = col[(m1+4)%5]+col[(m2+4)%5]
-            texto.append(text)
-            f = True
-    if f:
-        continue
-
-    i0=0
-    i1=0
-    j0=0
-    j1=0
-    for i in range(5):
-        fila = matriz[i]
-        if c1 in fila:
-            i0=i
-            j0=fila.find(c1)
-        if c2 in fila:
-            i1=i
-            j1=fila.find(c2)
-    text = matriz[i0][j1]+matriz[i1][j0]
-    texto.append(text)
-
-
-
-print('Texto: '+"".join(texto))
-    
-
-
+DesencriptarPlayfair(clav,cifrad)
 
 
 
