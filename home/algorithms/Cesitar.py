dcdbab48ea66e95d3a66e30e281e5d73ea0c9c46
Brayan Lipe <blipe@unsa.edu.pe>
2021-11-02 14:10:41 -0500

    Cesar's code 3.0

--- home/algorithms/Cesitar.py
+++ home/algorithms/Cesitar.py
@@ -5,7 +5,15 @@ alfabeto = list(string.ascii_uppercase)
 #alfabeto = list(string.ascii_lowercase)
 
 def cesar_cifrar(text, key):
-    text = text.upper()
+    cipher = ""
+    for i in range(len(text)): 
+        char = text[i]
+        if (char.islower()):
+            cipher += chr((ord(char) + key - 97) % 26 + 97) 
+        else:
+            cipher += chr((ord(char) + key - 65) % 26 + 65) 
+    return cipher
+    '''text = text.upper()
     coded_text = ''
     for letra in text:
         if letra in alfabeto:
@@ -14,15 +22,23 @@ def cesar_cifrar(text, key):
 
             if indice_cesar>27:
                 indice_cesar -= 27
+            print(indice_cesar)
             coded_text += alfabeto[indice_cesar]
         else:
             coded_text += letra
     
-    return coded_text
+    return coded_text'''
 
 def cesar_descifrar(text, key):
-    #text = text.upper()
-    decoded_text = ''
+    plain = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.islower()):
+            plain += chr((ord(char) -97-key) % 26 + 97)
+        else:
+            plain += chr((ord(char) -65-key) % 26 + 65)
+    return plain
+    '''decoded_text = ''
     for letra in text:
         if letra in alfabeto:
             indice_cesar = alfabeto.index(letra)
@@ -35,4 +51,26 @@ def cesar_descifrar(text, key):
         else:
             decoded_text += letra
     
-    return decoded_text
+    return decoded_text'''
+
+def brute_force(text):
+    for i in range(26):
+        print(i, ":: "+chr(65+i)+": "+ cesar_descifrar(text,i))
+    '''for key in range(len(alfabeto)):
+        translated = ''
+        for symbol in text:
+            if symbol in alfabeto:
+                num = alfabeto.index(symbol)
+                num = num - key
+            if num < 0:
+                num = num + len(alfabeto)
+            translated = translated + alfabeto[num]
+        else:
+            translated = translated + symbol
+        print('Hacking key #%s: %s' % (key, translated))'''
+    
+'''message = "Bota tu gaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas chupetin pipipipipipipipipii la recoooonn"
+m_c = cesar_cifrar(message, 50)
+print(m_c)
+#print(cesar_descifrar(message, 3))
+brute_force(m_c)'''
